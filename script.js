d2e503dad287e43bb86c40aac860ac5363c9b00e
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-02-04 12:03:25 +1300

    add np function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
 // basic NumPy arrays
 let array = document.getElementById("array");
+
+const np = () => {
+  if (array.value === "") {
+      return alert("Please enter a comma separated array in the 'array' field.");
+  } else if (variable.value === "") {
+     document.editor.textbox.value+= "\nnp.array([" + array.value.replaceAll(",", ", ").split(",") + "])";
+  } else if (Math.abs(Number(variable.value)) >= 0) {
+      return alert("Please do not enter a number in the 'variable' field.");
+  } else {
+     document.editor.textbox.value+= "\n" + variable.value + " = np.array([" + array.value.replaceAll(",", ", ").split(",") + "])";
+  }
+}
